362395155958192d7d08884d4553e257d6f2c67d
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2023-12-29 17:33:01 +0700

    add(task pertemuan13):refactor: handler & validation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # REPO TASK ACTIVITY COLLEGE BACKEND JAVA SCRIPT
 
-## pertemuan12
+## pertemuan13
 
 <p>Express JS Framework</p>
--- src/models/Student.js
+++ src/models/Student.js
@@ -13,13 +13,57 @@ class Student {
         })
     }
 
-    static createData(data) {
-        return new Promise((resolve, reject) => {
+    static async createData(data) {
+        const id = await new Promise((resolve, reject) => {
             db.query('INSERT INTO students SET ?', data, (err, result) => {
                 if (err) {
                     return reject(err)
                 } else {
                     return resolve('id : ' + result.insertId)
+                }
+            })
+        })
+
+        const student = await this.getDataById(id)
+        return student
+    }
+
+    static async updateDataById(id, data) {
+        await new Promise((resolve, reject) => {
+            db.query('UPDATE students SET ? WHERE id = ?', [data, id], (err, result) => {
+                if (err) {
+                    return reject(err)
+                } else {
+                    return resolve(result)
+                }
+            })
+        })
+
+        const student = await this.getDataById(id)
+        return student
+    }
+
+    static async destroyDataById(id) {
+        return new Promise((resolve, reject) => {
+            db.query('DELETE FROM students WHERE id = ?', id, (err, result) => {
+                if (err) {
+                    return reject(err)
+                } else {
+                    return resolve(result)
+                }
+            })
+        })
+    }
+
+    static async getDataById(id) {
+        return new Promise((resolve, reject) => {
+            db.query('SELECT * FROM students WHERE id = ?', id, (err, result) => {
+                const [student] = result
+
+                if (err) {
+                    return reject(err)
+                } else {
+                    return resolve(student)
                 }
             })
         })
